43d0bf6a03ea86131305560e72ef2282c829f88b
Rahul Pandita <rpandita1999@outlook.com>
2024-03-27 12:06:44 +0530

    write all available gemini models to a txt file

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ import os
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-for model in genai.list_models():
-    if "generateContent" in model.supported_generation_methods:
-        print(model.name)
+with open("models.txt", "w") as f:
+    for model in genai.list_models():
+        if "generateContent" in model.supported_generation_methods:
+            f.write(model.name)
+
