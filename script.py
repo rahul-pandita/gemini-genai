ae3bbea235e156916baedef1c2d67d26ce545c6d
Rahul Pandita <rpandita1999@outlook.com>
2024-03-27 13:09:31 +0530

    prompt api to parse image of calculus problem handwritten and return an answer; answer seems right :)

--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 import google.generativeai as genai
 import os
+import PIL.Image
+
+img = PIL.Image.open("calc.jpg")
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-pro-vision")
 
-response = model.generate_content("Write a poem on love in the style of the author Fyodor Dostoevsky")
+response = model.generate_content(["The following image contains an image of a mathematics problem written in human handwriting. Give a detailed step by step solution to the problem. Also identify the branch of mathematics to which this problem belongs", img])
 
 print(response.text)
